03b7cdede245181d83d92b6cfb3aab17366cbc28
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-27 22:25:27 +0600

    end

--- this.js
+++ this.js
@@ -42,6 +42,3 @@ function clickHandler() {
 document.getElementById("btn-click2").addEventListener("click", function () {
   console.log(this);
 });
-
-const obj = { a: 1, b: 7, c: 3, length: 2 };
-console.log(Object.keys(obj).length);
